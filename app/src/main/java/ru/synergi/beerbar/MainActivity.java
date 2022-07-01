658198332db175f7af98ac4bbf00f4baa6233121
OlegKarabanov <salem27@mail.ru>
2022-07-01 19:44:16 +0300

    Start project

---
+++ app/src/main/java/ru/synergi/beerbar/MainActivity.java
@@ -0,0 +1,21 @@
+package ru.synergi.beerbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.synergi.beerbar.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
